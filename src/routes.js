92aaf3445132a200dbe50df78283f2c14c2708e0
yuarmy0921 <christylin890921@gmail.com>
2020-12-18 16:50:08 +0800

    push

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 // GraphQL frontend
 import axios from 'axios';
 
-const instance = axios.create();
+const instance = axios.create({
+    baseURL: 'https://localhost:4000'
+});
 
 // Store 
-instance.get()
+const basicInfo = () => {
+    
+}
